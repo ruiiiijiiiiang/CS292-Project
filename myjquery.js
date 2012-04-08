dcd03d22092a0a67a6d227444f3abdbeddc6b5e1
curraheejerry@gmail.com <Rui Jiang>
2012-04-07 19:46:10 -0500

    merged main page

--- myjquery.js
+++ myjquery.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+
+  $("#home_content").show();
+  $("#tourguide_content").hide();
+  $("#plantseed_content").hide();
+  $("#forest_content").hide();
+
    // jQuery UI Accordion
   $(".accordion").accordion();
   
@@ -26,11 +32,31 @@ $(document).ready(function() {
 
   $("#tohome").click(function() {
     $("#home_content").show();
-    $("#touguide_content").hide();
+    $("#tourguide_content").hide();
     $("#plantseed_content").hide();
     $("#forest_content").hide();
   });
 
+  $("#totourguide").click(function() {
+    $("#home_content").hide();
+    $("#tourguide_content").show();
+    $("#plantseed_content").hide();
+    $("#forest_content").hide();
+  });
+
+  $("#toplantseed").click(function() {
+    $("#home_content").hide();
+    $("#tourguide_content").hide();
+    $("#plantseed_content").show();
+    $("#forest_content").hide();
+  });
+
+  $("#toforest").click(function() {
+    $("#home_content").hide();
+    $("#tourguide_content").hide();
+    $("#plantseed_content").hide();
+    $("#forest_content").show();
+  });
 
 });
 
