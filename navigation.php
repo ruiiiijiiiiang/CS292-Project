d2ecdeadc8a194aeaa71216d6320c366bd798e8f
ruiiiijiiiiang <Rui Jiang>
2012-04-15 17:38:32 -0500

    footer still broken

--- navigation.php
+++ navigation.php
@@ -1,10 +1,10 @@
 <html>
   <div id="navigation">
     <ul>
-      <li id="tohome"><a href="#">Home</a></li>
-      <li id="totourguide"><a href="#">Tour Guide</a></li>
-      <li id="toforest"><a href="#">The Forest</a></li>
-      <li id="toplantseed"><a href="#">Plant a Seed</a></li>
+      <li id="tohome"><a href="#">Home</a><span class="popup">Back to the main page</span></li>
+      <li id="totourguide"><a href="#">Tour Guide</a><span class="popup">Take a fast tour to learn how this site works</span></li>
+      <li id="toforest"><a href="#">The Forest</a><span class="popup">Explore the forest of stories</span></li>
+      <li id="toplantseed"><a href="#">Plant a Seed</a><span class="popup">Start your own story tree now</span></li>
  <!--    <li><a href="myprofile.php">My Profile</a></li> -->
     </ul>
   </div>
